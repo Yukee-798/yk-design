52bd6f4ae39645ad8cf82e840c8c29113cc8adad
Yukee-798 <909397116@qq.com>
2021-02-27 20:19:32 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import Hello from './components/Hello/Hello';
-import LikeButton from './components/LikeButton/LikeButton';
+
 
 import './App.css';
-import useMousePosition from './hooks/useMousePosition';
-// import MouseTracker from './components/MouseTracker/MouseTracker';
+
+
 
 
 function App() {
-  const [position, setPostion] = useMousePosition();
-  // const [a, b, c] = [1, 2, 3];
 
-  console.log(position);
+
   return (
     <div className="App">
-      <Hello message='Hello yk-design'/>
-      <LikeButton />
-      {/* <p>{`(${position.x}, ${position.y})`}</p> */}
+      hello
     </div>
   );
 }
