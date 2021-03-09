5cb0c0f2174706597cac7d22b66f86d4d2ba23a0
Yukee-798 <909397116@qq.com>
2021-03-09 23:26:37 +0800

    fix: type => btnType

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import classNames from 'classnames';
 
 
-export enum ButtonType {
+export enum ButtonTypes {
     Primary = 'primary',
     Default = 'default',
     Danger = 'danger',
     Link = 'link'
 }
 
-export enum ButtonSize {
+export enum ButtonSizes {
     Large = 'lg',
     Small = 'sm',
     Normal = 'nor'
@@ -22,24 +22,19 @@ type NativeButtonProps = React.ButtonHTMLAttributes<HTMLElement>;
 interface BaseButtonProps {
     className?: string;
     disabled?: boolean;
-    size?: ButtonSize;
+    size?: ButtonSizes;
     href?: string;
-    type?: 'primary' | 'default' | 'danger' | 'link' | 'submit' | 'reset' | 'button' | undefined;
+    btnType?: ButtonTypes
     children?: React.ReactNode;
 }
 
 export type ButtonProps = Partial<BaseButtonProps & NativeButtonProps & AnchorButtonProps>;
 
 
-// 我自己封装的 Button 的 props 接口里面的 type 属性和原生 button 标签里的 props 接口属性 type 冲突了
-// 原生接口中的 type?: 'submit' | 'reset' | 'button
-// 我自己封装的接口的 type:? 'primary' | 'default' | 'danger' | 'link'
-// 希望的 type:? 'submit' | 'reset' | 'button | 'primary' | 'default' | 'danger' | 'link'
-// 实际操作后的 type:? undefined
 
 const Button: React.FC<ButtonProps> = (props) => {
     const {
-        type,
+        btnType,
         disabled,
         size,
         href,
@@ -50,14 +45,14 @@ const Button: React.FC<ButtonProps> = (props) => {
 
     const classes = classNames('yk-btn', {
         // 如果传入的 props 中有 btnType 属性则会返回 'btn btn-primary(传入的 type)'
-        [`yk-btn-${type}`]: type,
+        [`yk-btn-${btnType}`]: btnType,
         [`yk-btn-${size}`]: size,
         'disabled': disabled,
         [`${className}`]: className
     });
 
     // btn-link
-    if (type === ButtonType.Link) {
+    if (btnType === ButtonTypes.Link) {
         return (
             <a 
                 className={classes} 
@@ -78,14 +73,14 @@ const Button: React.FC<ButtonProps> = (props) => {
                 {children}
             </button>
         )
-    };
+    }
 
 }
 
 Button.defaultProps = {
     disabled: false,
-    type: 'default',
-    size: ButtonSize.Normal,
+    btnType: ButtonTypes.Default,
+    size: ButtonSizes.Normal,
     children: 'button'
 }
 
