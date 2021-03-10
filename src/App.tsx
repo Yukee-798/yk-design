ce3a4d1bc3c49a050cf5774f0d098eaf4f95cce4
Yukee-798 <909397116@qq.com>
2021-03-10 21:59:19 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import Button, {ButtonTypes, ButtonSizes} from './components/Button/button'
+import './App.scss'
 function App() {
+
+  const handleClick = () => {
+    console.log(123);
+  }
   
   return (
     <div className="App">
-      hello
+
+      <div>
+        <Button btnType={ButtonTypes.Primary} onClick={handleClick}>primary</Button>
+        <Button>default</Button>
+        <Button btnType={ButtonTypes.Danger}>danger</Button>
+        <Button btnType={ButtonTypes.Link} href='#'>link</Button>
+      </div>
+
+      <div>
+        <Button disabled>default disabled</Button>
+        <Button disabled btnType={ButtonTypes.Link} href='#'>link disabled</Button>
+      </div>
+
+      <div>
+        <Button size={ButtonSizes.Large}>Large</Button>
+        <Button size={ButtonSizes.Normal}>Normal</Button>
+        <Button size={ButtonSizes.Small}>Small</Button>
+      </div>
+
     </div>
   );
 }
