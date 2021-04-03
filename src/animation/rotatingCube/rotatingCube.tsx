8dec8d336351f44fe42c58a443b30e80e8fbc09f
Yukee-798 <909397116@qq.com>
2021-04-03 16:15:28 +0800

    Update animation

---
+++ src/animation/rotatingCube/rotatingCube.tsx
@@ -0,0 +1,16 @@
+import './rotatingCube.scss';
+
+const RotatingCube = () => {
+    return (
+        <div className="container">
+            <div className="front">1</div>
+            <div className="back">2</div>
+            <div className="left">3</div>
+            <div className="right">4</div>
+            <div className="top">5</div>
+            <div className="bottom">6</div>
+        </div>
+    )
+}
+
+export default RotatingCube;
